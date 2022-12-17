b3e4d3946b497c03494d81ee11cad2471c51da9d
David Lei <DavidtheDefender2003@gmail.com>
2022-12-17 13:29:04 -0800

    Add HTML element variables

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+    HTML elements
+*/
+const changeDimensionsButton = document.querySelector(".change-dimensions-btn"); // Button to change canvas dimensions
+const canvasGrids = document.querySelector(".canvas-grids"); // Div that contains grids
+const colorSelector = document.querySelector(".color-selector"); // Color selector
+

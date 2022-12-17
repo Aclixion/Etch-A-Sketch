fa5e07434d1087f9306901ea6a7cdda47c71a371
David Lei <DavidtheDefender2003@gmail.com>
2022-12-17 14:54:39 -0800

    Add function to add grids to canvas

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const changeDimensionsButton = document.querySelector(".change-dimensions-btn");
 const canvasGrids = document.querySelector(".canvas-grids"); // Div that contains grids
 const colorSelector = document.querySelector(".color-selector"); // Color selector
 
+// Adds grids to the canvas
+function generateGrids(dimension) {
+    for (let i = 0; i < dimension; i++) {
+        for (let j = 0; j < dimension; j++) {
+            let grid = document.createElement("div");
+            grid.classList.add("grid");
+            canvasGrids.appendChild(grid);
+        }
+    }
+}
